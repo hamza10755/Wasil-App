214b9ef604a045282664a1b932c3c43b23f6dc6c
hamza10755 <202311944@students.asu.edu.jo>
2026-07-26 12:42:31 +0300

    scaffolded the entities and configured the connection string

--- Wasil.Api/Program.cs
+++ Wasil.Api/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Wasil.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<WasilDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
